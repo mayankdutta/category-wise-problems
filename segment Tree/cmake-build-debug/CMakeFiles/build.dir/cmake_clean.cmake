20bff6265832fdbaf93d7f7b05e0d3cfbcc2f928
sridharan-084 <ishwaranbabu@gmail.com>
2022-05-19 20:07:45 +0530

    segment Tree added
    segment Tree

---
+++ segment Tree/cmake-build-debug/CMakeFiles/build.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/build.dir/build.cpp.obj"
+  "build.exe"
+  "build.exe.manifest"
+  "build.pdb"
+  "libbuild.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/build.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
